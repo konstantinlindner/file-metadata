a30adbdb319edf25f63907d82b63d33b10e3ad15
Konstantin Lindner <hello@konte.se>
2024-04-30 14:48:11 -0700

    write csv script

--- src/scripts/getMetadataFromCSV.ts
+++ src/scripts/getMetadataFromCSV.ts
@@ -1,25 +1,54 @@
 import { readFileSync, readdirSync, writeFileSync } from 'fs';
-import crypto from 'crypto';
 import ffmpeg from 'fluent-ffmpeg';
-import { fileTypeFromBuffer } from 'file-type';
+import Papa from 'papaparse';
+
+type CSVRow = {
+  link: string | null;
+};
 
 export default async function getMetadataFromCSV() {
-  const files = readdirSync('input');
-  const results = [];
+  const csvFiles = readdirSync('csvInput');
 
-  for (const file of files) {
-    const filePath = `input/${file}`;
-    const fileBuffer = readFileSync(filePath);
+  if (csvFiles.length === 0) {
+    console.log('No CSV files found');
+    return;
+  }
 
-    const fileType = await fileTypeFromBuffer(fileBuffer);
-    const extension = fileType?.ext;
-    const mime = fileType?.mime;
+  if (csvFiles.length > 1) {
+    console.log('Multiple CSV files found, please only insert one at a time');
+    return;
+  }
 
-    const hash = crypto.createHash('sha256').update(fileBuffer).digest('hex');
+  const csvFilePath = `csvInput/${csvFiles[0]}`;
+  const csvString = readFileSync(csvFilePath, 'utf8');
+
+  const csv = Papa.parse<CSVRow>(csvString, {
+    header: true,
+  });
+
+  // check if the csv has a link header
+  if (!csv.meta.fields?.includes('link')) {
+    console.log('CSV file must have a "link" column');
+    return;
+  }
+
+  const rows = csv.data;
+
+  const results: unknown[] = [];
+
+  for (const row of rows) {
+    const link = row.link;
+
+    if (!link || link === 'NULL') {
+      console.log('Link is empty or null, skipping...');
+      continue;
+    }
+
+    console.log('processing link:', link);
 
     const duration: number | undefined = await new Promise(
       (resolve, reject) => {
-        ffmpeg.ffprobe(filePath, (err, metadata) => {
+        ffmpeg.ffprobe(link, (err, metadata) => {
           if (err) {
             reject(err);
           } else {
@@ -30,18 +59,18 @@ export default async function getMetadataFromCSV() {
     );
     const durationMs = duration ? Math.round(duration * 1000) : undefined;
 
-    const size = fileBuffer.byteLength;
-
     results.push({
-      file,
-      extension,
-      mime,
-      hash,
+      ...row,
+      link,
+      duration,
       durationMs,
-      size,
     });
   }
 
-  console.log('Finished processing files');
-  writeFileSync('output/output.json', JSON.stringify(results, null, 2));
+  const outputCsv = Papa.unparse(results);
+
+  writeFileSync('output/CSVOutput.csv', outputCsv);
+  writeFileSync('output/CSVOutput.json', JSON.stringify(results, null, 2));
+
+  console.log('Finished processing CSV');
 }
