4f0963a55fb64a70381a926799d7d60cc681a5e7
Konstantin Lindner <hello@konte.se>
2024-04-30 12:22:44 -0700

    break out script into own file

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,28 @@
-import { readFileSync, readdirSync, writeFileSync } from 'fs';
-import crypto from 'crypto';
-import ffmpeg from 'fluent-ffmpeg';
-import { fileTypeFromBuffer } from 'file-type';
+import { checkbox } from '@inquirer/prompts';
+import getMetadataFromFiles from './scripts/getMetadataFromFiles.js';
+import getMetadataFromCSV from './scripts/getMetadataFromCSV.js';
 
-async function getVideoInfo() {
-  const files = readdirSync('input');
-  const results = [];
+const questions = {
+  scripts: {
+    message: 'Select which scripts to run',
+    required: true,
+    choices: [
+      { name: 'Get metadata from video files', value: 'getMetadataFromFiles' },
+      { name: 'Get metadata from CSV', value: 'getMetadataFromCSV' },
+    ],
+  },
+};
 
-  for (const file of files) {
-    const filePath = `input/${file}`;
-    const fileBuffer = readFileSync(filePath);
+async function main() {
+  const selectedScripts = await checkbox(questions.scripts);
 
-    const fileType = await fileTypeFromBuffer(fileBuffer);
-    const extension = fileType?.ext;
-    const mime = fileType?.mime;
-
-    const hash = crypto.createHash('sha256').update(fileBuffer).digest('hex');
-
-    const duration: number | undefined = await new Promise(
-      (resolve, reject) => {
-        ffmpeg.ffprobe(filePath, (err, metadata) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(metadata.format.duration);
-          }
-        });
-      }
-    );
-    const durationMs = duration ? Math.round(duration * 1000) : undefined;
-
-    const size = fileBuffer.byteLength;
-
-    results.push({
-      file,
-      extension,
-      mime,
-      hash,
-      durationMs,
-      size,
-    });
+  if (selectedScripts.includes('getMetadataFromFiles')) {
+    await getMetadataFromFiles();
   }
 
-  console.log('Finished processing files');
-  writeFileSync('output/output.json', JSON.stringify(results, null, 2));
+  if (selectedScripts.includes('getMetadataFromCSV')) {
+    await getMetadataFromCSV();
+  }
 }
 
-getVideoInfo();
+main();
