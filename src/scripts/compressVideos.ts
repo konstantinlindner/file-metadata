f5daabbbeb040d534346ab60376620eff2ca4799
Konstantin Lindner <hello@konte.se>
2024-08-06 21:45:04 +0200

    add function to compress videos

---
+++ src/scripts/compressVideos.ts
@@ -0,0 +1,31 @@
+import { readdirSync } from 'fs';
+import ffmpeg from 'fluent-ffmpeg';
+
+export default async function compressVideos() {
+  console.log('Started compressing videos...');
+
+  const files = readdirSync('fileInput');
+
+  for (const file of files) {
+    const filePath = `fileInput/${file}`;
+
+    await new Promise<void>((resolve, reject) => {
+      ffmpeg(filePath)
+        .size('720x1280')
+        // .fps(29.97)
+        .addOptions(['-crf 30', '-preset veryslow'])
+        .on('end', () => {
+          console.log('Finished compressing video:', file);
+          resolve();
+        })
+        .on('error', (err) => {
+          console.log('An error occured while compressing video:', file);
+          console.log(err);
+          reject(err);
+        })
+        .save(`output/compressed-${file}`);
+    });
+  }
+
+  console.log('Finished compressing all videos');
+}
